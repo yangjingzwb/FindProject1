32a8114690102819be8e6978ad3ccfc4da71a641
杨静 <yangjing@bestpay.com.cn>
2018-06-21 15:31:26 +0800

    Aa

--- src/index/config/env.js
+++ src/index/config/env.js
@@ -43,7 +43,7 @@ if (debug == 1) { //生产
 } else { //客户端模拟
     // baseUrl.api = 'http://localhost:8000/rcServer/';//
     // baseUrl.api = 'http://www.ifruit.org/rcServer/';//
-    baseUrl.api = 'http://192.168.1.102:7300/mock/59cd9c4f55deb8dfcb498e17/mock/'
+    baseUrl.api = 'http://192.168.0.248:7300/mock/59cd9c4f55deb8dfcb498e17/mock/'
     baseUrl.img = 'https://mca.cmpay.com:28710'
     // baseUrl.img = 'https://mca.cmpay.com:28710'
     
